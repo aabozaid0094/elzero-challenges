373c96e6fff9a56eca0b5dbaad2ff97a2887fce1
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-10-20 19:32:18 +0200

    Adding Parmeters Random Order (Console)

--- index.js
+++ index.js
@@ -124,11 +124,41 @@ let switch_into_if = () => {
 console.log("switch_into_if", switch_into_if());
 
 // Leading and Following Existance
-let check_leading_following = (to_check, to_check_against)=>{
-    return (Array.isArray(to_check_against) && !isNaN(to_check)) ? (to_check_against.includes(to_check-1) && to_check_against.includes(to_check+1) ) : false ;
+let check_leading_following = (to_check, to_check_against) => {
+    return Array.isArray(to_check_against) && !isNaN(to_check)
+        ? to_check_against.includes(to_check - 1) &&
+              to_check_against.includes(to_check + 1)
+        : false;
 };
 
 let arrayOfNumbers = [1, 2, 4, 10, 20, 12];
 console.log(check_leading_following(11, arrayOfNumbers));
 console.log(check_leading_following(3, arrayOfNumbers));
 console.log(check_leading_following(5, arrayOfNumbers));
+
+// Parmeters Random Order
+let status = (parmeter1, parmeter2, parmeter3) => {
+    let parmeters = [parmeter1, parmeter2, parmeter3];
+    let variables = {
+        name: { type: "string", value: null },
+        age: { type: "number", value: null },
+        hireable: { type: "boolean", value: null },
+    };
+    parmeters.forEach((parmeter) => {
+        Object.keys(variables).forEach(function (key) {
+            if (typeof parmeter == variables[key].type) {
+                variables[key].value = parmeter;
+            }
+        });
+    });
+    console.log(
+        `Hello ${variables.name.value}, Age Is ${variables.age.value}, ${
+            !variables.hireable.value ? "Not " : ""
+        }Available For Hire`
+    );
+};
+
+status("Osama", 38, true); // "Hello Osama, Age Is 38, Available For Hire"
+status(38, "Osama", true); // "Hello Osama, Age Is 38, Available For Hire"
+status(true, 38, "Osama"); // "Hello Osama, Age Is 38, Available For Hire"
+status(false, "Osama", 38); // "Hello Osama, Age Is 38, Not Available For Hire"
