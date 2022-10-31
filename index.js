794424144c542b3a82582b08d9cd8a6345c1e449
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-10-31 19:26:57 +0200

    Adding Text Characters Location (Console)

--- index.js
+++ index.js
@@ -270,7 +270,6 @@ console.log("== Condition Met Challenge ==");
 let st = "Elzero Web School";
 if (
     st.split("").some((element) => {
-        console.log(element.toLowerCase());
         element.toLowerCase() === "w";
         return true;
     })
@@ -289,4 +288,18 @@ if (typeof st.length === "number") {
 if (st.split(" ").shift().repeat(2) === "ElzeroElzero") {
     console.log("Good");
 }
+console.log("===============================");
+
+// Text Characters Location
+console.log("== Text Characters Location ==");
+let word_to_nums = (word)=>{
+    const loweredWord = word.toLowerCase();
+    let charsNumbers = [];
+    for (let index = 0; index < loweredWord.length; index++) {
+        charsNumbers.push(loweredWord.charCodeAt(index) - 96);
+    }
+    return charsNumbers;
+};
+// Tests
+console.log('word_to_nums("Elzero")', word_to_nums("Elzero")); // "5 12 26 5 18 15"
 console.log("===============================");
