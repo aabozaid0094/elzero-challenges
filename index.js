4707131d99473a970312fddb12a520e2cecbacd2
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-10-31 19:09:09 +0200

    Adding Condition Met Challenge (Console)

--- index.js
+++ index.js
@@ -264,3 +264,29 @@ console.log(
         my[counter].slice(-zero).toUpperCase()
 ); // "rO"
 console.log("===============================");
+
+// Condition Met Challenge
+console.log("== Condition Met Challenge ==");
+let st = "Elzero Web School";
+if (
+    st.split("").some((element) => {
+        console.log(element.toLowerCase());
+        element.toLowerCase() === "w";
+        return true;
+    })
+) {
+    console.log("Good");
+}
+if ((st.toUpperCase().includes("W") ? "w" : "") === "w") {
+    console.log("Good");
+}
+if (typeof st.length !== "string") {
+    console.log("Good");
+}
+if (typeof st.length === "number") {
+    console.log("Good");
+}
+if (st.split(" ").shift().repeat(2) === "ElzeroElzero") {
+    console.log("Good");
+}
+console.log("===============================");
