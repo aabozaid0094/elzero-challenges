383c263f861f047377c2f667a0d36093175387e3
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2022-11-06 17:31:28 +0200

    Play With Variables Values

---
+++ scripts/easy/play_with_variables_values.js
@@ -0,0 +1,9 @@
+// Play With Variables Values
+console.log("== Play With Variables Values ==");
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+console.log(Math.abs(parseInt(d)) * parseInt(e)); // 2000
+console.log((Math.log10(Math.abs(parseInt(d))) * Math.abs(parseInt(d))) - f + (f/(Math.sqrt(Math.abs(parseInt(d)))))); // 173
+console.log("===============================");
